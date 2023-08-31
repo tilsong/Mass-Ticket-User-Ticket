3ba913919848f1289aa71995d987c1f5d700ad0b
tilsong <cnsong0229@gmail.com>
2023-08-31 16:29:20 +0900

    prj init

---
+++ src/main/java/com/massticketuserticket/MassTicketUserTicketApplication.java
@@ -0,0 +1,13 @@
+package com.massticketuserticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassTicketUserTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MassTicketUserTicketApplication.class, args);
+    }
+
+}
